b7300db419c231e2d5993832c11da4a92ebaa344
AmorLinux <ariel-gmm39@hotmail.com>
2026-07-02 15:48:07 -0500

    Flake limpio para la laptop

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
 {
-  description = "Mi sistema NixOS a medida - Multi-Host";
+  description = "Sistema NixOS - Laptop Base";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,17 +11,17 @@
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
-      
-      # ==========================================
-      # PERFIL 1: MÁQUINA VIRTUAL ACTUAL (PC 1)
-      # ==========================================
-      vm-principal = nixpkgs.lib.nixosSystem {
+      laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
-          ./hardware-vm-principal.nix # <-- Hardware específico de esta máquina
+          # El hardware real de tu VMware actual
+          ./hardware-configuration.nix
+          
+          # Tu configuración general del sistema
           ./configuration.nix
           
+          # La integración de Home Manager para tu usuario
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
@@ -31,26 +30,6 @@
           }
         ];
       };
-
-      # ==========================================
-      # PERFIL 2: NUEVA MÁQUINA VIRTUAL (PC 2)
-      # ==========================================
-      vm-secundaria = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hardware-vm-secundaria.nix # <-- Marcador de posición para la otra PC
-          ./configuration.nix
-          
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.amoreira = import ./home.nix;
-          }
-        ];
-      };
-
     };
   };
 }
